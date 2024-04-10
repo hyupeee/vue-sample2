77552b3939e73c38371f4771afbea9b14d779e56
이상협 <sanghyup@isanghyeob-ui-noteubug.local>
2024-04-10 18:15:32 +0900

    lint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,31 +15,31 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-import { Navigation, Pagination } from 'swiper'
-import { SwiperCore, Swiper, SwiperSlide } from 'swiper-vue2'
+import { Navigation, Pagination } from 'swiper';
+import { SwiperCore, Swiper, SwiperSlide } from 'swiper-vue2';
 import 'swiper/swiper-bundle.css';
 
-SwiperCore.use([Navigation, Pagination])
+SwiperCore.use([Navigation, Pagination,]);
 @Component({
-  name: 'Home',
-  components: {
-    Swiper,
-    SwiperSlide,
-  }
+	name: 'Home',
+	components: {
+		Swiper,
+		SwiperSlide,
+	},
 })
 export default class Home extends Vue {
 
-  protected mounted() {
-    console.log('HomeVue mounted');
-  }
+	protected mounted() {
+		console.log('HomeVue mounted');
+	}
 
-  public onSwiper () {
-    console.log('onswiper');
-  }
+	public onSwiper () {
+		console.log('onswiper');
+	}
 
-  public onSlideChange () {
-    console.log('onSlideChange');
-  }
+	public onSlideChange () {
+		console.log('onSlideChange');
+	}
 
 
 }
